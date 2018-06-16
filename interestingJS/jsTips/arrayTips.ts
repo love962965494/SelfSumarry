9c07a6cf545b35fba62237acdf10f7dc5df33c76
love962965494 <962965494@qq.com>
2018-06-16 11:55:56 +0800

    继续添加arrayTips和arrayPolyfill

--- interestingJS/jsTips/arrayTips.ts
+++ interestingJS/jsTips/arrayTips.ts
@@ -1,8 +1,7 @@
-
 export default class ArrayTips {
   /**
    * @function: chunk 数组分块
-   * @description: 
+   * @description:
    *   把一个数组分成指定大小的小数组
    *   使用Array.from()创建一个新的数组，它的长度就是生成chunk块的数量。
    *   使用Array.slice()将新数组的每个元素映射到长度为size的chunk中。
@@ -16,7 +15,9 @@ export default class ArrayTips {
   chunk(arr: Array<any>, size: number): Array<any> {
     // Array.from(obj, mapFn) => Array.from(obj).map(mapFn)
     // Array.from({length: 5}, (v, i) => i) => [0, 1, 2, 3, 4]
-    return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => arr.slice(i * size, i * size + size))
+    return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+      arr.slice(i * size, i * size + size),
+    );
   }
 
   /**
@@ -30,7 +31,7 @@ export default class ArrayTips {
    * @memberof ArrayTips
    */
   compact(arr: Array<any>): Array<any> {
-    return arr.filter(Boolean)
+    return arr.filter(Boolean);
   }
 
   /**
@@ -46,9 +47,9 @@ export default class ArrayTips {
    */
   countBy(arr: Array<any>, fn): object {
     return arr.map(typeof fn === 'function' ? fn : val => val[fn]).reduce((acc, val: any, i) => {
-      acc[val] = (acc[val] || 0) + 1
-      return acc
-    }, {})
+      acc[val] = (acc[val] || 0) + 1;
+      return acc;
+    }, {});
   }
 
   /**
@@ -63,29 +64,116 @@ export default class ArrayTips {
    * @memberof ArrayTips
    */
   countOccurrences(arr: Array<any>, val): number {
-    return arr.reduce((a, v) => (v === val ? a + 1 : a + 0), 0)
+    return arr.reduce((a, v) => (v === val ? a + 1 : a + 0), 0);
+  }
+
+  /**
+   * @function: deepFlatten 深度平铺数组
+   * @description:
+   *   深度平铺一个数组
+   *   使用递归。通过空数组([])使用Array.concat()，结合展开运算符(...)来平铺数组。递归平铺每个数组元素。
+   *
+   * @param {Array<any>} arr
+   * @returns {Array<any>}
+   * @memberof ArrayTips
+   */
+  deepFlatten(arr: Array<any>): Array<any> {
+    return [].concat(...arr.map(v => (Array.isArray(v) ? this.deepFlatten(v) : v)));
+  }
+
+  /**
+   * @function: difference 数组比较
+   * @description:
+   *   返回两个数组之间的差异
+   *   根据数组arr2创建一个Set对象，然后在数组arr1上使用Array.filter()方法，过滤出数组arr2中不包含的值。
+   *
+   * @param {Array<any>} arr1
+   * @param {Array<any>} arr2
+   * @returns {Array<any>}
+   * @memberof ArrayTips
+   */
+  difference(arr1: Array<any>, arr2: Array<any>): Array<any> {
+    const s = new Set(arr2);
+    return arr1.filter(x => !s.has(x));
+  }
+
+  /**
+   * @function: differenceWith 通过比较函数比较两个数组的差异
+   * @description:
+   *   过滤出数组中比较函数不返回true的所有值。类似于difference，除了接受一个comparator（比较函数）
+   *   使用Array.filter()和Array.findIndex()来查找合适的值
+   *
+   * @param {Array<any>} arr
+   * @param {Array<any>} val
+   * @param {Function} comp
+   * @returns {Array<any>}
+   * @memberof ArrayTips
+   */
+  differenceWith(arr: Array<any>, val: Array<any>, comp: Function): Array<any> {
+    return arr.filter(a => val.findIndex(b => comp(a, b)) === -1);
+  }
+
+  /**
+   * @function: distinctValuesOfArray 数组去重
+   * @description:
+   *   返回数组的所有不同值
+   *   使用ES6的Set和...rest操作符剔除重复的值
+   *
+   * @param {Array<any>} arr
+   * @returns {Array<any>}
+   * @memberof ArrayTips
+   */
+  distinctValuesOfArray(arr: Array<any>): Array<any> {
+    return [...new Set(arr)]
   }
 }
 
-const arrayTips = new ArrayTips()
+const arrayTips = new ArrayTips();
 
 /**
  * example of chunk
  */
-console.log('chunkL: ', arrayTips.chunk([1, 2, 3, 4, 5], 2))
+console.log('chunkL: ', arrayTips.chunk([1, 2, 3, 4, 5], 2));
 
 /**
  * example of compact
  */
-console.log('compact: ', arrayTips.compact([0, 1, false, 2, '', 3, 'a', NaN, 's', 34]))
+console.log('compact: ', arrayTips.compact([0, 1, false, 2, '', 3, 'a', NaN, 's', 34]));
 
 /**
  * example of countBy
  */
-console.log('countBy: ', arrayTips.countBy([6.1, 4.2, 6.3], Math.floor))
-console.log('countBy: ', arrayTips.countBy(['one', 'two', 'three'], 'length'))
+console.log('countBy: ', arrayTips.countBy([6.1, 4.2, 6.3], Math.floor));
+console.log('countBy: ', arrayTips.countBy(['one', 'two', 'three'], 'length'));
 
 /**
  * example of countOccurrences
  */
-console.log('countOccurrences: ', arrayTips.countOccurrences([1, 2, 1, 1, 2, 3, 1], 1))
+console.log('countOccurrences: ', arrayTips.countOccurrences([1, 2, 1, 1, 2, 3, 1], 1));
+
+/**
+ * example of deepFlatten
+ */
+console.log('deepFlatten: ', arrayTips.deepFlatten([1, [2], [[3], 4, 5]]));
+
+/**
+ * example of difference
+ */
+console.log('difference: ', arrayTips.difference([1, 2, 3], [2, 4, 5]));
+
+/**
+ * example of differenceWith
+ */
+console.log(
+  'differenceWith: ',
+  arrayTips.differenceWith(
+    [1, 1.2, 1.5, 3, 0],
+    [1.9, 3, 0],
+    (a, b) => Math.round(a) === Math.round(b),
+  ),
+);
+
+/**
+ * example of distinctValuesOfArray
+ */
+console.log('distinctValuesOfArray: ', arrayTips.distinctValuesOfArray([1, 2, 2, 3, 4, 3, 5]))
--- interestingJS/polyfill/arrayPolyfill.ts
+++ interestingJS/polyfill/arrayPolyfill.ts
@@ -1,86 +1,141 @@
 export default class arrayPolyfill {
+  /**
+   * @function: find(arrayLike, mapFn?: Function, thisArg?)，根据arrayLike返回一个新的数组
+   * @description:
+   *   Array上的方法，如果参数中mapFn是函数的话，则新创建的数组每个元素都会调用一次该函数
+   *   类似于：Array.from(arrayLike).map(mapFn)
+   *
+   * @memberof arrayPolyfill
+   */
   from() {
     if (!Array.from) {
-      Array.from = (function(){
-        var toStr = Object.prototype.toString
+      Array.from = (function() {
+        var toStr = Object.prototype.toString;
         var isCallable = function(fn) {
-          return typeof fn === 'function' || toStr.call(fn) === '[object Function]'
-        }
+          return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+        };
         var toInteger = function(value) {
-          var number = Number(value)
-          
+          var number = Number(value);
+
           if (isNaN(number)) {
-            return 0
+            return 0;
           }
 
           if (number === 0 || !isFinite(number)) {
-            return number
+            return number;
           }
 
-          return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number))
-        }
-        var maxSageInteger = Math.pow(2, 53) - 1
+          return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+        };
+        var maxSageInteger = Math.pow(2, 53) - 1;
         var toLength = function(value) {
-          var len = toInteger(value)
-          
-          return Math.min(Math.max(len, 0), maxSageInteger)
-        }
+          var len = toInteger(value);
+
+          return Math.min(Math.max(len, 0), maxSageInteger);
+        };
 
         // the length property of the from method is 1
-        return function from(arrayLike/*, mapFn, thisArg*/) {
+        return function from(arrayLike /*, mapFn, thisArg*/) {
           // let _that be this value
-          var _that = this
+          var _that = this;
           // let items be ToObject(arrayLike)
-          var items = Object(arrayLike)
+          var items = Object(arrayLike);
 
           // ReturnIfAbrupt(items)
           if (arrayLike === null) {
-            throw new TypeError('Array.from requires an array-like object - not null or undefined')
+            throw new TypeError('Array.from requires an array-like object - not null or undefined');
           }
 
           // if mapFn is undefined, then let mapping be false
-          var mapFn = arguments.length > 1 ? arguments[1] : void undefined
-          var T
+          var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+          var T;
 
           if (typeof mapFn !== 'undefined') {
             // if IsCallable(mapFn) is false, throw a TypeError exception
             if (!isCallable(mapFn)) {
-              throw new TypeError('Array.from: when provided, the second argument must be a function')
+              throw new TypeError(
+                'Array.from: when provided, the second argument must be a function',
+              );
             }
 
             // is thisArg was supplied, let T be thisArg; else let T be undefined
             if (arguments.length > 2) {
-              T = arguments[2]
+              T = arguments[2];
             }
           }
 
           // let lenValue be Get(items, 'length')
           // let len be ToLength(lenValue)
-          var len = toLength(items.length)
+          var len = toLength(items.length);
 
           // if IsConstructor(_that) is true, then
           // a: let A be the result of calling the [[Constructor]] internal method
           // of _that with an argument list containing the single item len
           // b: else let a be ArrayCreate(len)
-          var result = isCallable(_that) ? Object(new _that(len)) : new Array(len)
+          var result = isCallable(_that) ? Object(new _that(len)) : new Array(len);
 
-          var k = 0
-          var kValue
+          var k = 0;
+          var kValue;
 
           while (k < len) {
-            kValue = items[k]
+            kValue = items[k];
             if (mapFn) {
-              result[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k)
+              result[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
             } else {
-              result[k] = kValue
+              result[k] = kValue;
             }
-            k += 1
+            k += 1;
           }
-          result.length = len
+          result.length = len;
 
-          return result
-        }
-      }())
+          return result;
+        };
+      })();
+    }
+  }
+
+  /**
+   * @function: findIndex(predicate: Function, thisArg?) 找到数组中第一个复合要求的元素，返回其索引
+   * @description:
+   *   Array.prototype上的方法
+   *
+   *
+   * @memberof arrayPolyfill
+   */
+  findIndex() {
+    if (!Array.prototype.findIndex) {
+      Object.defineProperty(Array.prototype, 'findIndex', {
+        value: function(predicate) {
+          if (this == null) {
+            throw new TypeError('"this" is null or undefined');
+          }
+
+          var o = Object(this);
+          var len = o.length >>> 0;
+
+          if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+          }
+
+          var thisArg = arguments[1];
+          var k = 0;
+
+          while (k < len) {
+            // a. Let Pk be ! ToString(k)
+            // b. Let kValue be ? Get(O, Pk)
+            // c. Let testResult be ToBoolean(? Call(predicate, T, << kValue, k, O >>))
+            // d. If testResult is true, return k
+            var kValue = o[k];
+
+            if (predicate.call(thisArg, kValue, k, o)) {
+              return k;
+            }
+            k++;
+          }
+
+          return -1;
+        },
+      });
     }
   }
 }
